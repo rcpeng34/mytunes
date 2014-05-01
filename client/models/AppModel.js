6adb64b1a3016e4a1e09a8550cd6d42c2c0eb480
Hack Reactor Students <rcpeng@gmail.com>
2014-04-30 20:24:52 -0700

    Adds ability to add songs to queue and display the queue

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -14,7 +14,12 @@ var AppModel = Backbone.Model.extend({
 
 
     params.library.on('play', function(song){
+
       this.set('currentSong', song);
+    }, this);
+
+    params.library.on('enqueue', function(song){
+      this.get('songQueue').push(song);
     }, this);
   }
 
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,25 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+
+    this.collection.on('add', function(){
+      //this adds a listener to SongQueueView
+      //appends a SongQueueEntryView with the model = song
+      this.render();
+    }, this);
   },
 
   render: function() {
-    return this.$el;
-  }
+    // return this.$el;
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  },
+
 
 });
